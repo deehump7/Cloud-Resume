d1e56582352ae0778824483c5fdc6725e74fd965
Delreka "Dee" Humphries <98293808+deehump7@users.noreply.github.com>
2026-04-24 15:16:33 -0400

    Update main.js
    update function code

--- main.js
+++ main.js
@@ -71,22 +71,18 @@ document.querySelectorAll('.scroll-section').forEach(s => observer.observe(s));
 // ===========================
 // VISITOR COUNTER
 // ===========================
-async function updateCounter() {
+async function getVisitorCount() {
   try {
-    const response = await fetch("https://visitor-counter-g3a7htaze9cfdbga.canadacentral-01.azurewebsites.net/api/GetVisitorCount");
-
-    if (!response.ok) {
-      throw new Error("Counter fetch failed");
-    }
-
-    const data = await response.json();
-    document.getElementById("counter").innerText = data.count;
-  } catch (error) {
-    console.error("Visitor counter error:", error);
+    const res = await fetch("https://visitor-counter-g3a7htaze9cfdbga.canadacentral-01.azurewebsites.net/api/GetVisitorCount");
+;
+   const data = await res.json();
+     document.getElementById("visitor-count").textContent = data.visitorCount;
+  } catch (err) {
+     console.error("Visitor count failed:" , err);
+     document.getElementByID("visitor-count").textContent = "N/A";
   }
 }
-
-updateCounter();
+getVisitorCount();
 
 // ===========================
 // MAGIC CURSOR TRAIL
